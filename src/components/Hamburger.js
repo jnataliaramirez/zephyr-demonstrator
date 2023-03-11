d55e7d825cad21bfecae80b04150916a8e25d1ea
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-03-11 21:04:07 +0100

    Do logic menu hamburger

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -7,8 +7,11 @@ const Hamburger = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="hamburger" onClick={() => setOpen(!open)} >
-      <div className="hamburger__line"></div>
+    <div
+      className={`hamburger ${open && "active"}`}
+      onClick={() => setOpen(!open)}
+    >
+      <div className={`hamburger__line ${open && "active"}`}></div>
     </div>
   );
 };
