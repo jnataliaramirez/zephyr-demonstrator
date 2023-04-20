1575d2871852c515c5a67df65b021d6e420ecbea
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-04-20 18:01:18 +0200

    Clean code Hamburguer

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -1,15 +1,12 @@
-import React from "react";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../index.css";
 import "./styles.css";
 
 const Hamburger = () => {
-  const [open, setOpen] = useState(false);
 
   return (
     <Link to="/">
-      <div className="hamburger" onClick={() => setOpen(!open)}>
+      <div className="hamburger">
         <div className="hamburger__line"></div>
       </div>
     </Link>
